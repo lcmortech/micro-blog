6e639f3cbb56ec5f60afbcafef74404f86424170
Lauren <rg326@live.com>
2022-01-10 16:35:55 -0500

    fixed bug in post route

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
 const app = express();
+app.use(bodyParser.json());
 
 const post = {};
 
@@ -7,8 +11,15 @@ app.get('/posts',(req, res)=> {
     res.send(posts);
 });
 
-app.post('/posts'(req, res)=> {
+app.post('/posts',(req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
 
+    post[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id]);
 });
 
 app.listen(4000, ()=>{
